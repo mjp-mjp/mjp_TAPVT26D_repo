9a74cfe5b4bee8222eee06bc0aeeb29fd87c7b18
mjp <283464876+mjp-mjp@users.noreply.github.com>
2026-05-24 11:24:31 +0200

    Add gitignore and main_mjp.py for week 1

---
+++ Veckouppgift-1-MJP/main_mjp.py
@@ -0,0 +1,8 @@
+# Veckouppgift 1
+# 1 Starta upp PyCharm - check
+# 2 Skapa projekt och synka med GitHub - check
+
+message = "Hello world"
+name = "MJP"
+print(message)
+print("This program was made by " + (name) + ".")
